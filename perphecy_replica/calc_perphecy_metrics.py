54d124edc2e0dd4da28bf0cb2dd2e733d0290cf5
Kevin Hannigan <kph1958@rit.edu>
2018-08-17 15:18:42 -0400

    Removing now obsolete folder

--- perphecy_replica/calc_perphecy_metrics.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-
-def calc_perphecy_metrics(commitsha_1, commitsha_2):
-    client = MongoClient()
-    db = client.research
-
-    dynamic_data_1 = db.commitprofiles.find_one({ "commitsha": commitsha_1 })
-    dynamic_data_2 = db.commitprofiles.find_one({ "commitsha": commitsha_2 })
